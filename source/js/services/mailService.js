7cdd142e81b5954169a9db33dc3ec277d9d5418c
glennvanhaute <glennvanhaute@gmail.com>
2015-11-16 10:12:40 +0100

    commentaar added

--- source/js/services/mailService.js
+++ source/js/services/mailService.js
@@ -1,6 +1,8 @@
 app.service("mailService", function(){
-    this.sendRapport = function(send){
-            console.log("works");
+    //hier komt Mandrill API call
 
+    this.sendRapport = function(send){
+        console.log("works");
+        // hier komt de send naar API
     }
 })
