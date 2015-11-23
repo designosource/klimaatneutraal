a0c74c98f46c05c641ad1fae6aa8810ffaec7bf6
glenn van haute <glenn van haute>
2015-11-23 23:39:22 +0100

    temporary push as backup, need to optimise code

--- source/js/services/mailService.js
+++ source/js/services/mailService.js
@@ -1,8 +1,36 @@
-angular.module('klimaatneutraal.services').service("mailService", function(){
+angular.module('klimaatneutraal.services').service("mailService",['$http', 'KEYS',
+    
+    function($http, KEYS){
     //hier komt Mandrill API call
 
     this.sendRapport = function(send){
-        console.log("works");
+        console.log("works");      
+        var fromEmail = 'email';
+
         // hier komt de send naar API
+        }
     }
-})
+          return $http.post('https://mandrillapp.com/api/1.0//messages/send.json', {
+            'key': KEYS.mandrill,
+            'message': {
+              'html': '<p>Unknown Error Message</p><p>' + resp + '</p><p>Error Code:' + resp.code + '</p>',
+              'text': resp,
+              'subject': 'Unknown Error Message',
+              'from_email': fromEmail,
+              'from_name': fromName,
+              'to': [
+              {
+                'email': toEmail,
+                'name': toName,
+                'type': 'to'
+              }
+              ],
+              'headers': {
+                'Reply-To': replyTo
+              }
+            }
+          });
+          
+                                                                  
+                                                                  
+]);
