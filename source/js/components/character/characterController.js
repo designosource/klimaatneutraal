05640eb7108d6dcffd4a13136c4f64e9c93d4b7b
Jasper De Smet <jasp.desmet@gmail.com>
2015-12-03 17:19:07 +0100

    Added custom characters

--- source/js/components/character/characterController.js
+++ source/js/components/character/characterController.js
@@ -8,26 +8,40 @@ angular.module('klimaatneutraal.controllers')
 
             var init = function() {
                 console.log('characterController');
+
                 $scope.character = {
                     sex: '1',
                     age: '1',
-                    skin: '1'
+                    skin: '1',
+                    mood: '1'
                 };
+
+                updateImage();    
             };
 
             var changeSex = function(sex) {
                 console.log(sex);
                 $scope.character.sex = sex;
+
+                updateImage();
             };
 
             var changeAge = function(age) {
                 console.log(age);
                 $scope.character.age = age;
+
+                updateImage();
             };
 
             var changeSkin = function(skin) {
                 console.log(skin);
                 $scope.character.skin = skin;
+
+                updateImage();
+            };
+
+            var updateImage = function() {
+                $scope.image = $scope.character.sex + $scope.character.age + $scope.character.skin + $scope.character.mood + '.svg';
             };
 
             $scope.changeSex = changeSex;
