bf9c3215c68fe60ef2fa22b3597139124938d2dc
glenn van haute <glenn van haute>
2015-12-08 17:38:43 +0100

    mandrill works, looking for alternative

--- source/js/components/end/endGameController.js
+++ source/js/components/end/endGameController.js
@@ -8,34 +8,13 @@ angular.module('klimaatneutraal.controllers')
         'mailService',
 
         function($rootScope, $scope, $state, $controller, $http, mailService) {
-            var init = function() {
-            };
 
-            init();
             
             $scope.sendTheMail = function() {
-                console.log("clicked");
-                mailService.sendRapport(/*send*/);
-
-                var m = new mandrill.Mandrill('NJR9rKU89OzZDcAKt7izMw');
-                // Collect Inputs
-                var email = document.getElementById('userEmail').value;
-                var name = document.getElementById('userName').value;
-                var subject = document.getElementById('userSubject').value;
-                var message = document.getElementById('userMessage').value;
-                var emailBody = "From: " + name + "<br><br>" +  + "Subject: " + subject + "<br><br>" + message;
-
-                var params = {
-
-                    "message": {
-                        "from_email":email,
-                        "to":[{"email":"glennvanhaute@gmail.com"}],
-                        "subject": "New email from website",
-                        "html": emailBody
-                    }
-                };
-
-                m.messages.send(params);
+                var emailUser = $("#userEmail").val();
+                
+/*                mailService.sendMandrill('name', 'firstName', emailUser);
+*/                mailService.sendMailchimp('name', 'firstName', emailUser);
 
                     };
                 }
--- source/js/services/mailService.js
+++ source/js/services/mailService.js
@@ -1,8 +1,56 @@
-angular.module('klimaatneutraal.services').service("mailService", function(){
-    //hier komt Mandrill API call
+angular.module('klimaatneutraal.services').service("mailService",[
+	'$http',
 
-    this.sendRapport = function(send){
-        console.log("works");
-        // hier komt de send naar API
-    }
-})
+	function($http){
+		var emailNeutraal = 'glennvanhaute@gmail.com';
+	 	var mailchimpKey='33b98fb66515f34a2391ca79edb49851-us12';
+	 	var mandrillKey='NJR9rKU89OzZDcAKt7izMw';
+
+	    var sendMandrill = function(name, firstName, email){
+	      	$http.post('https://mandrillapp.com/api/1.0//messages/send.json', {
+	            'key': mandrillKey,
+	            'message': {
+	              'html': 'je hebt goed gescoord',
+	              'text': 'resp',
+	              'subject': 'Resultaat klimaatneutraal',
+	              'from_email': emailNeutraal,
+	              'from_name': 'klimaatneutraal',
+	              'to': [
+	              {
+	                'email': email,
+	                'name': name,
+	                'type': 'to'
+	              }
+	              ],
+	              'headers': {
+	                'Reply-To': emailNeutraal
+	              }
+	            }
+
+	          });
+	    };
+
+
+      var sendMailchimp = function(name, firstName, email){
+/*	      	$http.post('https://us12.api.mailchimp.com/2.0/lists/subscribe.json?apikey='+mailchimpKey+'&id=cb553f7b01&email[email]='+email+'&merge_vars[FNAME]='+firstName+'&merge_vars[LNAME]='+name+'&double_optin=false&send_welcome=false');
+*/
+	      	$http.post('https://us12.api.mailchimp.com/3.0/lists/cb553f7b01/members', {
+	      		"key": mailchimpKey,
+			    "email_address": email,
+			    "status": "subscribed",
+			    "merge_fields": {
+			        "FNAME": "firstName",
+			        "LNAME": "name"
+			    }
+	        });
+      };
+
+      return {
+      	sendMandrill:sendMandrill,
+      	sendMailchimp:sendMailchimp
+      }
+
+        
+      return 
+    
+}]);
